56c2ddb715c0e692db91ee7f6be8f1910f2bffe4
Francky-DT-12 <153033070+Francky-DT-12@users.noreply.github.com>
2025-10-18 10:31:55 +0100

    DEBUG Change

--- projet_tech/settings.py
+++ projet_tech/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['projet-tech-1.onrender.com','localhost', '127.0.0.1']
 
