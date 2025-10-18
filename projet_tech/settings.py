fd1190afc7aa11b1931303f64e35e11ae768113d
Francky-DT-12 <153033070+Francky-DT-12@users.noreply.github.com>
2025-10-18 10:26:26 +0100

    Restrict ALLOWED_HOSTS for production environment

--- projet_tech/settings.py
+++ projet_tech/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['projet-tech-1.onrender.com'']
 
 
 # Application definition
